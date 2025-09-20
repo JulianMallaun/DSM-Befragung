dd193d3d33431c9a3331b2b3b6da6dbfb0ccfa0b
JulianMallaun <julianmall@live.com>
2025-09-20 20:01:11 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-# Streamlit App Code kommt hier rein...
+import streamlit as st
+
+st.set_page_config(page_title="Befragung Lastflexibilität – Hotel", page_icon="🏨", layout="centered")
+
+st.title("Befragung Lastflexibilität – Hotel")
+st.write("Dies ist die finale Version der Befragungs-App.")
